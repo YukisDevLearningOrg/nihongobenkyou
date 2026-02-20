df51332528dca7640a4848cebe8fc2011a10ab0b
Yukihara11 <225840574+Yukihara11@users.noreply.github.com>
2026-02-20 22:52:19 +0100

    created HomeRoute.tsx and added path to const router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router'
+import App from "./App.tsx";
+import HomeRoute from "./routes/HomeRoute.tsx";
 
 
-const router = createBrowserRouter([])
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                index: true,
+                element: <HomeRoute/>
+            }
+        ]
+
+    }
+])
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error("Root element missing")
